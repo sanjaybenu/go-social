ec3e53d561edc631d9226e33b14ceb8bf4eaabc1
S Chopra <sanjaybenu@gmail.com>
2023-06-25 22:41:12 +1000

    added models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const { isEmail } = require("validator");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "Username is Required",
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is Required",
+      validate: [isEmail, "Not a valid Email"],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendsCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
